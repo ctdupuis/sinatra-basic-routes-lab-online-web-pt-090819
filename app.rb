b8ae71c6f54d8c9140e076c230364128681e80d0
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-19 21:33:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is __"
+  end
+  
+  get '/hometown' do
+    
+  end
+  
+  get '/favorite-song' do
+    
+  end
 end
